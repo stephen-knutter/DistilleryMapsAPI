32f00b9a1181cbad0b488338111322071e5cf450
smkqp8 <stephen.knutter@gmail.com>
2016-11-28 15:30:18 -0700

    update access control

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/users', users);
 app.use('/distills', distills);
 
 // Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
 // Request methods you wish to allow
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
