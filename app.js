d02b136128a56d17fd795154dbe65baf8ad20cb7
smkqp8 <stephen.knutter@gmail.com>
2016-11-28 15:38:39 -0700

    add next middleware

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use((req, res, next) => {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
 })
 
 // catch 404 and forward to error handler
