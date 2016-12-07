1275fa89ba94974ad34188dd6f4e145449a2b2d2
smkqp8 <stephen.knutter@gmail.com>
2016-12-06 19:09:04 -0700

    update seeds

--- migrations/20161203204932_add_ratings.js
+++ migrations/20161203204932_add_ratings.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('ratings', (t) => {
     t.increments();
-    t.integer('distill_id').references('id').inTable('distilleries').notNullable();
-    t.integer('user_id').references('id').inTable('users').notNullable();
+    t.integer('distill_id').notNullable();
+    t.integer('user_id').notNullable();
     t.integer('rating').notNullable();
     t.text('comment').notNullable();
     t.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
