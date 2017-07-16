c94898d5c3fd93a501eb94d2407dd5b851f972c9
smkqp8 <stephen.knutter@gmail.com>
2017-07-16 15:01:24 -0600

    opwerp

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 'use-strict';
-const fs = require('fs');
 
-require('dotenv').config(/*{path: __dirname + '/.env'}*/);
+// require('dotenv').config(/*{path: __dirname + '/.env'}*/);
 
-console.log('node env: ', process.env.NODE_ENV == 'production');
-fs.readdir('../' + __dirname, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-})
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
