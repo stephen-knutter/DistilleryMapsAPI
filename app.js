ebeeec98e341d000e6a516b1bbe0cc53fadc0cd6
smkqp8 <stephen.knutter@gmail.com>
2017-07-16 17:28:21 -0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use-strict';
 
-require('dotenv').config(/*{path: __dirname + '/.env'}*/);
+require('dotenv').config({path: __dirname + '/.env'});
 
 const express = require('express');
 const path = require('path');
